47654c4971885e37d4c997968666fbf85fe9f1aa
adam <139786308+Addz89@users.noreply.github.com>
2023-12-04 10:37:59 +1100

    Update contact.php

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $mobile = $_POST["mobile"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "your-email@example.com";
+    $subject = "New Contact Form Submission: $subject";
+    $headers = "From: $email";
+
+    // Compose the email message
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Mobile: $mobile\n";
+    $email_message .= "Subject: $subject\n\n";
+    $email_message .= "Message:\n$message";
+
+    // Send the email
+    mail($to, $subject, $email_message, $headers);
+}
+?>
