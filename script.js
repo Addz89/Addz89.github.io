d86b0e65dc267c3e2167b376049579c4850c4e57
adam <139786308+Addz89@users.noreply.github.com>
2023-12-04 23:43:39 +1100

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,13 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
         }
+        // if want to use animation the repeats on scroll use this
+            //  else {
+                //  sec.classList.remove('show-animate');
+        //  }
     })
     // sticky header
     let header = document.querySelector('header');
@@ -34,5 +40,10 @@ window.onscroll = () => {
     // remove toggle icon and navbar when click navbar links(scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.sc);
 
 }
